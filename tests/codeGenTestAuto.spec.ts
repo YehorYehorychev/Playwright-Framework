9aa99e79fb036743fd4b9a49543c9ebcb676cb28
YehorYehorychev <egor57899@gmail.com>
2024-04-03 19:16:56 -0700

    Auto-Generated test with automated file saving created and 2 previous test  were deleted

---
+++ tests/codeGenTestAuto.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.google.com/');
+  await page.frameLocator('iframe[name="callout"]').getByLabel('Stay signed out').click();
+  await page.getByLabel('Search', { exact: true }).click();
+  await page.getByLabel('Search', { exact: true }).fill('ruby pro');
+  await page.getByText('gramming language', { exact: true }).click();
+  await page.getByRole('link', { name: 'Ruby Programming Language Ruby Programming Language https://www.ruby-lang.org' }).click();
+  await page.getByRole('link', { name: 'Read More...' }).click();
+  await page.getByRole('link', { name: 'Ruby A Programmer\'s Best' }).click();
+});
