4bae1cea6627ae1a6774bd003e87b165d83247c7
YehorYehorychev <egor57899@gmail.com>
2024-04-05 15:48:46 -0700

    AssertionTest Enable/Disable button created

--- tests/assertionTest.spec.ts
+++ tests/assertionTest.spec.ts
@@ -9,10 +9,17 @@ test('Visible/Hidden Assertion', async ({ page }) => {
     await page.close();
 })
 
-test.only('Present/Not Present Assertion', async ({ page }) => {
+test('Present/Not Present Assertion', async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/add_remove_elements/');
     await expect(page.locator('.added-manually')).not.toHaveCount(1);
     await page.locator('button[onclick="addElement()"]').click();
     await expect(page.locator('.added-manually')).toHaveCount(1);
+    await page.close();
+})
+
+test.only('Enable/Disable Assertion', async ({ page }) => {
+    await page.goto('https://letcode.in/buttons');
+    await expect(page.locator('#property')).toBeEnabled();
+    await expect(page.locator('[title="Disabled button"]')).toBeDisabled();
     await page.close();
 })
