65f6cbca3725c4d33e8a0e47f5808e332a13f9dc
YehorYehorychev <egor57899@gmail.com>
2024-04-05 17:42:32 -0700

    Press - Sequentially test added

--- tests/textBoxHandling.spec.ts
+++ tests/textBoxHandling.spec.ts
@@ -10,3 +10,13 @@ test('Fill Method', async ({ page }) => {
     await page.locator('text=Logout').click();
     await page.close();
 })
+
+test.only('Press - Sequentially', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com');
+    await page.locator('input[placeholder="Username"]').pressSequentially('Admin');
+    await page.locator('input[placeholder="Password"]').pressSequentially('admin123');
+    await page.locator('input[placeholder="Password"]').press('Enter');
+    await page.locator('.oxd-userdropdown-tab').click();
+    await page.locator('text=Logout').click();
+    await page.close();
+})
