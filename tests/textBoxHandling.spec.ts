3515ffaa3e1a5288e9e5001e6dbb75d75a4cdcd0
YehorYehorychev <egor57899@gmail.com>
2024-04-05 17:13:14 -0700

    Fill Method test added

---
+++ tests/textBoxHandling.spec.ts
@@ -0,0 +1,12 @@
+import { test } from '@playwright/test'
+
+test('Fill Method', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com');
+    await page.locator('input[placeholder="Username"]').fill('Admin');
+    await page.locator('input[placeholder="Password"]').fill('admin123');
+    await page.locator('input[placeholder="Password"]').press('Enter');
+    await page.locator('(//span[@class="oxd-userdropdown-tab"])[1]').click();
+    await page.locator('.oxd-userdropdown-tab').click();
+    await page.locator('text=Logout').click();
+    await page.close();
+})
