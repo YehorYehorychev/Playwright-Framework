a9676dfbc14b9b31f552455c736172bec0df137b
YehorYehorychev <egor57899@gmail.com>
2024-04-06 10:18:53 -0700

    Right Click test added

--- tests/buttonHandling.spec.ts
+++ tests/buttonHandling.spec.ts
@@ -6,14 +6,14 @@ test('Regular Button single click', async ({ page }) => {
     await expect(page.locator('#click_type')).toHaveText('Click');
 });
 
-test.only('Double Click', async ({ page }) => {
+test('Double Click', async ({ page }) => {
     await page.goto('https://play1.automationcamp.ir/mouse_events.html');
     await page.locator('#click_area').dblclick();
     await expect(page.locator('#click_type')).toHaveText('Double-Click');
 });
 
-// test('Right Click', async ({ page }) => {
-//     await page.goto('https://play1.automationcamp.ir/mouse_events.html');
-    
-
-// });
+test.only('Right Click', async ({ page }) => {
+    await page.goto('https://play1.automationcamp.ir/mouse_events.html');
+    await page.locator('#click_area').click({button:'right'});
+    await expect(page.locator('#click_type')).toHaveText('Right-Click');
+});
