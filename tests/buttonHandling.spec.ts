67d7f0a7d0092530dff73f71d85581dac0f15cf2
YehorYehorychev <egor57899@gmail.com>
2024-04-06 10:16:28 -0700

    Double-Click test added

--- tests/buttonHandling.spec.ts
+++ tests/buttonHandling.spec.ts
@@ -1,15 +1,16 @@
 import { test, expect } from '@playwright/test'
 
-test.only('Regular Button single click', async ({ page }) => {
+test('Regular Button single click', async ({ page }) => {
     await page.goto('https://play1.automationcamp.ir/mouse_events.html');
     await page.locator('#click_area').click();
     await expect(page.locator('#click_type')).toHaveText('Click');
 });
 
-// test('Double Click', async ({ page }) => {
-//     await page.goto('https://play1.automationcamp.ir/mouse_events.html');
-
-// });
+test.only('Double Click', async ({ page }) => {
+    await page.goto('https://play1.automationcamp.ir/mouse_events.html');
+    await page.locator('#click_area').dblclick();
+    await expect(page.locator('#click_type')).toHaveText('Double-Click');
+});
 
 // test('Right Click', async ({ page }) => {
 //     await page.goto('https://play1.automationcamp.ir/mouse_events.html');
