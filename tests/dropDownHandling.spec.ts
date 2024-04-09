4f60cfc43de23eb41d1a18c2cb46288db48b6f2c
YehorYehorychev <egor57899@gmail.com>
2024-04-08 17:51:26 -0700

    Single Static DropDown Handling test added

---
+++ tests/dropDownHandling.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test'
+
+test('Single Static DropDown Handling', async ({ page }) => {
+    await page.goto('https://demo.automationtesting.in/Register.html');
+
+    await page.selectOption('#Skills', {
+        value: "Android"
+    })
+
+    await page.pause();
+
+    await page.selectOption('#Skills', {
+        label: "Art Design"
+    })
+
+    await page.pause();
+
+    await page.selectOption('#Skills', {
+        index: 3
+    })
+
+    await page.pause();
+    await page.close();
+});
