a196dda8e195983740822782466ebbe0cc5dab4c
YehorYehorychev <egor57899@gmail.com>
2024-04-08 18:56:11 -0700

    Class AlertHandling created and Simple Alert Handling test added (Second iteration)

--- tests/alertHandling.spec.ts
+++ tests/alertHandling.spec.ts
@@ -2,7 +2,15 @@ import { test, expect } from '@playwright/test'
 
 test('Simple Alert Handling', async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+    
+    page.on("dialog", async (alert) => {
+        const alertMessage = alert.message();
+        expect(alertMessage).toEqual('I am a JS Alert');
+        await alert.accept();
+        await expect(page.locator('#result')).toHaveText('You successfully clicked an alert');
+    })
     await page.locator('button[onclick="jsAlert()"]').click();
-    await expect(page.locator('#result')).toHaveText('You successfully clicked an alert');
+    // await page.locator('button[onclick="jsAlert()"]').click();
+    // await expect(page.locator('#result')).toHaveText('You successfully clicked an alert');
     await page.close();
 });
