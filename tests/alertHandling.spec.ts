db0e07f2b07e39267dd486f7e6dc88bd7c74a758
YehorYehorychev <egor57899@gmail.com>
2024-04-08 19:01:46 -0700

    Confirm Alert test added

--- tests/alertHandling.spec.ts
+++ tests/alertHandling.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 
 test('Simple Alert Handling', async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
-    
+
     page.on("dialog", async (alert) => {
         const alertMessage = alert.message();
         expect(alertMessage).toEqual('I am a JS Alert');
@@ -12,5 +12,19 @@ test('Simple Alert Handling', async ({ page }) => {
     await page.locator('button[onclick="jsAlert()"]').click();
     // await page.locator('button[onclick="jsAlert()"]').click();
     // await expect(page.locator('#result')).toHaveText('You successfully clicked an alert');
+    await page.close();
+});
+
+test('Confirm Alert - OK Button', async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+
+    page.on("dialog", async (alert) => {
+        const alertMessage = alert.message();
+        expect(alertMessage).toEqual('I am a JS Confirm');
+        await alert.accept();
+        await expect(page.locator('#result')).toHaveText('You clicked: Ok');
+    })
+
+    await page.locator('button[onclick="jsConfirm()"]').click();
     await page.close();
 });
