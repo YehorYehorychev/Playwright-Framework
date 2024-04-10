9648bc9983bd9ec58ec2d7ec5af6546d9672a591
YehorYehorychev <egor57899@gmail.com>
2024-04-09 18:38:37 -0700

    Web Table Handling test added (1st Iteration)

--- tests/webTableHandling.spec.ts
+++ tests/webTableHandling.spec.ts
@@ -1,7 +1,19 @@
 import { test, expect } from '@playwright/test'
 
-test('Web Table Handling', async ({ page }) => {
+test.only('Web Table Handling', async ({ page }) => {
     await page.goto('https://testautomationpractice.blogspot.com/');
+    const table = page.locator('table[name="BookTable"]');
+
+    // Total rows and columns
+    const columns = table.locator('tr th');
+    console.log("Number of Columns: ", await columns.count());
+
+    const rows = table.locator('tbody tr');
+    console.log("Number of Rows: ", await rows.count());
+
+    expect(await columns.count()).toBe(4);
+    expect(await rows.count()).toBe(7);
+
 
     await page.close();
 });
