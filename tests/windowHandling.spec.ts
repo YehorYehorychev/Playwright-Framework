01da6d6e8ce55f858b824cd0160c7023404a4f25
YehorYehorychev <egor57899@gmail.com>
2024-04-10 21:38:44 -0700

    Multiple Tab Handling test added(First Iteration)

--- tests/windowHandling.spec.ts
+++ tests/windowHandling.spec.ts
@@ -32,3 +32,37 @@ test('Single Window Handling', async ({ page }) => {
 
     await page.close();
 });
+
+test.only('Multiple Tab Handling', async ({ page }) => {
+    await page.goto('https://demo.automationtesting.in/Windows.html');
+    await page.locator('.analystic[href="#Multiple"]').click();
+
+    const [multipleTab] = await Promise.all([
+        page.waitForEvent('popup'),
+        await page.click('button[onclick="multiwindow()"]')
+    ])
+
+    await multipleTab.waitForLoadState();
+    const pages = multipleTab.context().pages();
+    console.log("Total pages opened:" + pages.length);
+    await multipleTab.close();
+
+    const [newPage] = await Promise.all([
+        page.waitForEvent('popup'),
+        page.click('button[onclick="multiwindow()"]')
+    ]);
+
+    await newPage.waitForLoadState();
+    await newPage.fill('//*[@id="email"]', 'egor@yahoo.com');
+    // await page[1].locator('input[placeholder="Email id for Sign Up"]').fill('egor@yahoo.com');
+    // await multipleTab.waitForTimeout(2000);
+
+    // await pages[2].waitForTimeout(2000);
+    // await pages[2].locator('#navbarDropdown').click();
+    // await pages[2].locator('a[href="/about"]').click();
+    // await multipleTab.waitForTimeout(2000);
+
+    // await pages[1].close();
+    // await pages[2].close();
+    await page.close();
+});
