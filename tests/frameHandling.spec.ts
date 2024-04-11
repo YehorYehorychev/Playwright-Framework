93a51de4d66bfa587b576bb178bd57c5da32d6b2
YehorYehorychev <egor57899@gmail.com>
2024-04-10 20:15:46 -0700

    Nested Frame Handling test added

--- tests/frameHandling.spec.ts
+++ tests/frameHandling.spec.ts
@@ -21,3 +21,30 @@ test('Frame Handling using Page.FrameLocator()', async ({ page }) => {
     await page.waitForTimeout(3000);
     await page.close();
 });
+
+test.only('Nested Frame Handling', async ({ page }) => {
+    await page.goto('https://ui.vision/demo/webtest/frames/');
+    await page.waitForLoadState('networkidle');
+
+    const frame3 = page.frame({ url: 'https://ui.vision/demo/webtest/frames/frame_3.html' });
+
+    if (frame3) {
+        const childFrames = frame3.childFrames();
+        if (childFrames && childFrames.length > 0) {
+            await childFrames[0].waitForLoadState('networkidle');
+
+            const checkbox1 = await childFrames[0].locator('//*[@id="i8"]/div[3]/div');
+            const checkbox2 = await childFrames[0].locator('//*[@id="i19"]/div[3]');
+
+            if (checkbox1 && checkbox2) {
+                if (!checkbox1.isChecked())
+                    await checkbox1.check({ force: true });
+
+                if (!checkbox2.isChecked())
+                    await checkbox2.check({ force: true });
+            }
+        }
+    }
+    await page.waitForTimeout(3000);
+    await page.close();
+});
