d59df1b34acaf7b0446417dca8d2d585c7021bb4
YehorYehorychev <egor57899@gmail.com>
2024-04-16 19:14:33 -0700

    Home page objects added for the Page object Model demo

---
+++ tests/pages/homePage.ts
@@ -0,0 +1,14 @@
+import { Locator, Page } from "@playwright/test";
+
+export class HomePage {
+    readonly page: Page;
+    readonly settingsButton: Locator;
+
+    constructor(page: Page) {
+        this.settingsButton = page.locator('a[href="#settings"]');
+    }
+
+    async clickSettingsButton() {
+        await this.settingsButton.click();
+    }
+}
