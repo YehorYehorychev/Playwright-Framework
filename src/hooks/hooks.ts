36b1971e52073baaa18357617dbe8f7d303c3bcb
YehorYehorychev <egor57899@gmail.com>
2024-04-23 18:20:33 -0700

    Cucumber Report with Screenshots final functionality added

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,4 +1,4 @@
-import { After, AfterAll, Before, BeforeAll, Status } from '@cucumber/cucumber';
+import { After, AfterAll, AfterStep, Before, BeforeAll, Status } from '@cucumber/cucumber';
 import { Browser, BrowserContext, Page, chromium } from '@playwright/test';
 import { pageFixture } from './pageFixture';
 
@@ -18,7 +18,7 @@ Before( async function() {
 });
 
 After( async function({ pickle, result }) {
-    // Screenshots of Failed Test Cases only
+   // Screenshots of Failed Test Cases only
     if(result?.status == Status.FAILED) {
         const image = await pageFixture.page.screenshot({
             path:`./cucumber-test-result/screenshots/${pickle.name}.png`, type: "png"
@@ -26,9 +26,23 @@ After( async function({ pickle, result }) {
         await this.attach(image, "image/png");
     }
 
+    // Screenshot after each test
+        // const image = await pageFixture.page.screenshot({
+        //     path:`./cucumber-test-result/screenshots/${pickle.name}.png`, type: "png"
+        // });
+        // await this.attach(image, "image/png");
+
     await page.close();
     await context.close();
 });
+
+// AfterStep( async function({ pickle, result }) {
+//     // Screenshot after each step
+//     const image = await pageFixture.page.screenshot({
+//         path:`./cucumber-test-result/screenshots/${pickle.name}.png`, type: "png"
+//     });
+//     await this.attach(image, "image/png");
+// });
 
 AfterAll( async function() {
     await browser.close();
